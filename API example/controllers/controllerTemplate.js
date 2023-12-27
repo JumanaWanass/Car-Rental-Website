2f6344ddf8f2357b6ec73e15b532088ec9d0f8b7
Abdullah Elsayed <abdallah.elsaid25@gmail.com>
2023-12-27 14:26:48 +0200

    Add Office

--- API example/controllers/controllerTemplate.js
+++
@@ -1,8 +0,0 @@
-const Lawyer = require('./../models/ModelTemplate');
-
-// exports mean that it can be imported
-
-exports.function = 
-async (req, res) => {
-    res.status(code).json(JSON_response)
-}
--- API example/middleware/middleware.js
+++
@@ -1,6 +0,0 @@
-function authenticate(req, res, next) {
-    res.status(code).send(json) // next means don't continue something wrong
-    next(); // next means all good
-}
-
-module.exports = authenticate;
