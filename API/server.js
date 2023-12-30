3f22b5016ff1db88c0e39f5f4c9d95487d2d811f
Salma <salmaelmahy@hotmail.com>
2023-12-30 02:27:11 +0200

    Get updates

--- API/server.js
+++ API/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const app = express();  //we're hosting
 
 // Body parser
 app.use(express.urlencoded({ extended: false }));
