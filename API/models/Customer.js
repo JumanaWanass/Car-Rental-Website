af1d53d255a9816ffc01fce1ffd69f489c8c15eb
Jumana Wanass <jumanawanass@hotmail.com>
2024-01-02 01:25:20 +0200

    Fixed adminsearch oxm bellah

--- API/models/Customer.js
+++ API/models/Customer.js
@@ -84,7 +84,7 @@ class Customer
 
     static async createCustomer(customer) {
         // Validate registration before creating the customer
-        const validation = await validateRegistration(customer.Email, customer.SSN, customer.CountryName);
+        const validation = await this.validateRegistration(customer.Email, customer.SSN, customer.CountryName);
     
         if (!validation.success) {
             return { success: false, error: validation.message };
@@ -110,14 +110,14 @@ class Customer
     static async validateRegistration(email, ssn, country) 
     {
         // Check if the email is unique
-        const [emailResult] = await findByAttributes({ email });
+        const [emailResult] = await this.findByAttributes({ email });
         if (emailResult.length > 0) 
         {
             return { success: false, message: 'Email is already registered.' };
         }
     
         // Check if the SSN is unique considering the country
-        const [ssnResult] = await findByAttributes({ ssn });
+        const [ssnResult] = await this.findByAttributes({ ssn });
         if (ssnResult.length > 0) 
         {
             const existingCountry = ssnResult[0].country;
